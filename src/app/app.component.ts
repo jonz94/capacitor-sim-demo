396cebe8c18cb67c0919895e5cbd92a2a8eb5198
jonz94 <jody16888@gmail.com>
2023-02-27 21:00:17 +0800

    feat: show basic device information

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,25 @@
-import { JsonPipe, NgIf } from '@angular/common';
+import { AsyncPipe, JsonPipe, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { Device, DeviceInfo } from '@capacitor/device';
 import { IonicModule } from '@ionic/angular';
 import { Sim, SimCard } from '@jonz94/capacitor-sim';
+import { from, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonicModule, NgIf, JsonPipe],
+  imports: [IonicModule, NgIf, AsyncPipe, JsonPipe],
 })
 export class AppComponent {
   protected simCards: SimCard[] = [];
+
+  protected basicDeviceInformation$ = from(Device.getInfo()).pipe(
+    map(
+      ({ platform, osVersion, manufacturer, model }) =>
+        ({ platform, osVersion, manufacturer, model } satisfies Partial<DeviceInfo>),
+    ),
+  );
 
   async getSimCards() {
     const { simCards } = await Sim.getSimCards();
